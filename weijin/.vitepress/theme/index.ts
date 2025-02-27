d503d757bd212fc509278b1273d40b825dd7fa02
weijin <xfn016069>
2025-02-27 14:49:51 +0800

    chore: 添加vitepress主题配置

---
+++ weijin/.vitepress/theme/index.ts
@@ -0,0 +1,9 @@
+import DefaultTheme from 'vitepress/theme'
+import { type Theme } from 'vitepress'
+
+import './styles/vars.css'
+
+export default {
+    extends: DefaultTheme,
+    // 这里可以添加自定义主题配置
+} satisfies Theme
